0d720410d21e050e70b1f930f309f9e8dbb70cc1
hasan <mdhasanhk.98@gmail.com>
2023-03-29 13:19:54 +0600

    first commit

---
+++ starter/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.continents}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.ben}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.BDT.name}</p>
+  </div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('Bangladesh');
+getCountryData('Germany');
