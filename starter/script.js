acf4df6a77dbad6e6492fac9ac32a83966664bfc
hasan <mdhasanhk.98@gmail.com>
2023-04-27 11:30:14 +0600

    async await

--- starter/script.js
+++ starter/script.js
@@ -374,3 +374,17 @@ const whereAmI = function () {
 btn.addEventListener('click', whereAmI);
 ///////////////////////////////////////
 */
+
+const whereAmI = async function (country) {
+  // fetch(`https://restcountries.com/v3.1/name/${country}`).then(res =>
+  //   console.log(res)
+  // );
+
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  console.log(res);
+  const data = await res.json();
+  console.log(data);
+  renderCountry(data[0]);
+};
+whereAmI('bangladesh');
+console.log('First');
